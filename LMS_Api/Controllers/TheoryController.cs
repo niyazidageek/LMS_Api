4a38f758bb41ff694bb3991138589c639549f468
Niyazi Babayev <niyazibabayev@Niyazis-MacBook-Pro.local>
2021-11-12 14:16:48 +0400

    Theory assessment added

--- LMS_Api/Controllers/TheoryController.cs
+++ LMS_Api/Controllers/TheoryController.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Business.Abstract;
+using DataAccess.Identity;
 using Entities.DTOs;
 using Entities.Models;
 using LMS_Api.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -20,11 +23,26 @@ namespace LMS_Api.Controllers
         private readonly ITheoryService _theoryService;
         private readonly ILessonService _lessonService;
         private readonly IMapper _mapper;
+        private readonly IGroupMaxPointService _groupMaxPointService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITheoryAppUserService _theoryAppUserService;
+        private readonly IAppUserGroupService _appUserGroupService;
+        private readonly IAppUserGroupPointService _appUserGroupPointService;
 
         public TheoryController(ITheoryService theoryService,
             ILessonService lessonService,
-            IMapper mapper)
+            IAppUserGroupService appUserGroupService,
+            IAppUserGroupPointService appUserGroupPointService,
+            IMapper mapper,
+            IGroupMaxPointService groupMaxPointService,
+            ITheoryAppUserService theoryAppUserService,
+            UserManager<AppUser> userManager)
         {
+            _appUserGroupPointService = appUserGroupPointService;
+            _appUserGroupService = appUserGroupService;
+            _groupMaxPointService = groupMaxPointService;
+            _theoryAppUserService = theoryAppUserService;
+            _userManager = userManager;
             _mapper = mapper;
             _theoryService = theoryService;
             _lessonService = lessonService;
@@ -41,7 +59,7 @@ namespace LMS_Api.Controllers
 
             var theoriesDb = await _theoryService.GetTheoriesByLessonIdAsync(id);
 
-            var theoriesDto =  _mapper.Map<List<TheoryDTO>>(theoriesDb);
+            var theoriesDto = _mapper.Map<List<TheoryDTO>>(theoriesDb);
 
             return Ok(theoriesDto);
         }
@@ -67,11 +85,42 @@ namespace LMS_Api.Controllers
 
             var theoryDb = _mapper.Map<Theory>(theoryDto);
 
+            var lessonDb = await _lessonService.GetLessonByIdAsync(theoryDto.LessonId);
+
+            if (lessonDb is null)
+                return NotFound();
+
             var fileName = await FileHelper.AddJsonFile(theoryAttachmentDto.Content);
 
             theoryDb.FileName = fileName;
 
             await _theoryService.AddTheoryAsync(theoryDb);
+
+            List<AppUserGroup> students = new();
+
+            foreach (var appUserGroup in lessonDb.Group.AppUserGroups)
+            {
+                var user = await _userManager.FindByIdAsync(appUserGroup.AppUserId);
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var isStudent = roles.Any(x => x.ToLower() == nameof(Roles.Student).ToLower());
+
+                if (isStudent is true)
+                    students.Add(appUserGroup);
+            }
+
+            lessonDb.Group.AppUserGroups = students;
+
+            await _theoryAppUserService.InitializeTheoryAsync(lessonDb, theoryDb.Id);
+
+            var groupId = lessonDb.GroupId;
+
+            var groupMaxPoint = await _groupMaxPointService.GetGroupMaxPointByGroupId(groupId);
+
+            groupMaxPoint.MaxPoint += theoryDb.Point;
+
+            await _groupMaxPointService.EditGroupMaxPoint(groupMaxPoint);
 
             return Ok();
         }
@@ -84,6 +133,10 @@ namespace LMS_Api.Controllers
 
             if (theoryDb is null)
                 return NotFound();
+
+            var lessonDb = await _lessonService.GetLessonByIdAsync(theoryDb.LessonId);
+
+            var oldGrade = theoryDb.Point;
 
             var theoryDto = JsonConvert.DeserializeObject<TheoryDTO>(theoryAttachmentDto.Values);
 
@@ -98,6 +151,14 @@ namespace LMS_Api.Controllers
 
             await _theoryService.EditTheoryAsync(theoryDb);
 
+            var groupId = lessonDb.GroupId;
+
+            var groupMaxPoint = await _groupMaxPointService.GetGroupMaxPointByGroupId(groupId);
+
+            groupMaxPoint.MaxPoint = groupMaxPoint.MaxPoint - oldGrade + theoryDb.Point;
+
+            await _groupMaxPointService.EditGroupMaxPoint(groupMaxPoint);
+
             return Ok();
         }
 
@@ -110,9 +171,63 @@ namespace LMS_Api.Controllers
             if (theoryDb is null)
                 return NotFound();
 
+            var lessonDb = await _lessonService.GetLessonByIdAsync(theoryDb.LessonId);
+
             FileHelper.DeleteFile(theoryDb.FileName);
 
+            var currentGrade = theoryDb.Point;
+
+            var theoryAppUsers = await _theoryAppUserService.GetTheoryAppUsersByLessonIdAsync(lessonDb.Id);
+
+            foreach (var theoryAppUser in theoryAppUsers)
+            {
+                var appUserGroup = await _appUserGroupService
+                    .GetAppUserGroupByUserIdAndGroupIdAsync(theoryAppUser.AppUserId, lessonDb.GroupId);
+
+                var appUserGroupPoint = await _appUserGroupPointService
+                    .GetAppUserGroupPointByAppUserGroupIdAsync(appUserGroup.Id);
+
+                appUserGroupPoint.Point -= currentGrade;
+
+                await _appUserGroupPointService.EditAppUserGroupPointAsync(appUserGroupPoint);
+            }
+
             await _theoryService.DeleteTheoryAsync(id);
+
+            var groupId = lessonDb.GroupId;
+
+            var groupMaxPoint = await _groupMaxPointService.GetGroupMaxPointByGroupId(groupId);
+
+            groupMaxPoint.MaxPoint -= currentGrade;
+
+            await _groupMaxPointService.EditGroupMaxPoint(groupMaxPoint);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [Authorize(Roles = nameof(Roles.Student))]
+        public async Task<ActionResult> MarkTheoryAsRead(int id)
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == "uid").Value;
+            if (userId is null)
+                return Unauthorized();
+
+            var theoryDb = await _theoryService.GetTheoryByIdAsync(id);
+
+            if (theoryDb is null)
+                return NotFound();
+
+            var theoryAppUserDb = await _theoryAppUserService
+                .GetTheoryAppUserByTheoryIdAndUserIdAsync(id, userId);
+
+            if (theoryAppUserDb.IsRead is true)
+                return BadRequest();
+
+            theoryAppUserDb.IsRead = true;
+
+            await _theoryAppUserService.EditTheoryAppUserAsync(theoryAppUserDb);
 
             return Ok();
         }
